d195776768f8bfc04c5c0b8ec681d3cd8b809ce5
acliu1498 <alexanderchen.liu@calbaptist.edu>
2018-03-13 21:59:02 -0700

    2nd push works correctly

--- ReversiSolution.py
+++ ReversiSolution.py
@@ -5,12 +5,15 @@ import sys
 
 
 def main():
+    """Returns a new board with the move having taken place"""
+
     # Gets input
     args = parse_arguments()
     begin_state = json.load(args.infile)
 
     # Gets the current board
     board = begin_state['board']
+
     # sets move equal to the given move input
     move = begin_state['move']
     make_move(move, board)
@@ -24,21 +27,24 @@ def main():
 def make_move(move, board):
     """ this checks every direction from starting to see if a capture has been made"""
 
-    # dictionary of directions
-    directions = {1: 'U', 2: 'D', 3: 'L', 4: 'R'}
+    # a map of how to check in each direction
+    dir_map = {'U': -8, 'D': 8, 'L': -1, 'R': 1, 'DL': 7, 'DR': 9, 'UL': -7, 'UR': -9}
 
     # gets x and y from column and row
     x = move['column'] - 1
     y = move['row'] - 1
-    # runs make_move_helper for every direction
+
+    # finds the starting index and places a piece there
     start = x + (8 * (y % 8))
     board[start] = move['player']
-    for key in directions:
-        make_move_help(start, board, directions[key], start, move['player'])
+
+    # runs make_move_helper for every direction
+    for key in dir_map:
+        make_move_help(start, board, key, dir_map, start, move['player'])
 
 
-def make_move_help(start, board, direction, curr, player):
-    """ Helper method for make_move that recurses in a certain
+def make_move_help(start, board, direction, dir_map, curr, player):
+    """ Helper method for make_move that recurses in a given direction
     direction until the end of the board is reached, a player piece is reached
     or a 0 is reached"""
 
@@ -48,22 +54,11 @@ def make_move_help(start, board, direction, curr, player):
         if board[curr] == player and curr != start:
             return True
 
-        # if U moves up elif D moves down elif L moves left else moves right
-        if direction == 'U':
-            curr -= 8
-        elif direction == 'D':
-            curr += 8
-        elif direction == 'L':
-            if curr % 8 == 0:
-                return False
-            curr -= 1
-        else:
-            if curr % 8 == 7:
-                return False
-            curr += 1
+        # moves according to the map
+        curr += dir_map[direction]
 
         # makes a recursive call and checks if true
-        if make_move_help(start, board, direction, curr, player):
+        if make_move_help(start, board, direction, dir_map, curr, player):
             # if true sets current value to player
             board[curr] = player
             return True
